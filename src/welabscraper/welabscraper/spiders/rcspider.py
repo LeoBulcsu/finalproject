4a774d6fdf45abfccf2aba6c10857930f4124db1
LeoBulcsu <miguelcuadrosmoronta@gmail.com>
2023-11-29 12:52:58 +0100

    "update"

--- src/welabscraper/welabscraper/spiders/rcspider.py
+++ src/welabscraper/welabscraper/spiders/rcspider.py
@@ -3,28 +3,29 @@ import scrapy
 # RC SERVICE SCRAP
 
 class RCLensSpider(scrapy.Spider):
-    
+
     name = "rclensSpider"
     start_urls = ["https://www.rcservice.es/es/alquiler-opticas-cine-peliculas"]
 
     def parse(self, response):
         # Parse lens categories
 
-        rclens_categories = response.css('div.uk-clearfix > p a::attr(href)')
+        rclens_categories = response.css('div.uk-clearfix > p a::attr(href)').extract()
         
-        for category in rclens_categories:
+        for i in range(len(rclens_categories)):
 
-            category_url = category.extract()
+            category_url = rclens_categories[i]
             
             yield scrapy.Request(category_url, callback=self.parse_brands)
 
     def parse_brands(self, response):
         # Parse brands within categories
         
-        lens_brands = response.css('div.uk-clearfix > p a::attr(href)')
+        lens_brands = response.css('div.uk-clearfix > p a::attr(href)').extract()
         
-        for brand in lens_brands:
-            category_url = brand.extract()
+        for i in range(len(lens_brands)):
+            
+            category_url = lens_brands[i]
             
             yield scrapy.Request(category_url, callback=self.parse_products)
 
