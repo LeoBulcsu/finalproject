7fefec12293ead8df7978c4e9de83844b3d61744
LeoBulcsu <miguelcuadrosmoronta@gmail.com>
2023-11-29 17:58:09 +0100

    update"

--- src/welabscraper/welabscraper/spiders/avisualspider.py
+++ src/welabscraper/welabscraper/spiders/avisualspider.py
@@ -2,38 +2,33 @@ import scrapy
 
 # RC SERVICE SCRAP
 
-class avisualLensSpider_1(scrapy.Spider):
+class avisualSpider(scrapy.Spider):
 
-    name = "avisuallensSpider"
+    name = "avisualSpider"
     start_urls = ["(https://www.avisualpro.es/categoria-producto/opticas)"]
 
     def parse(self, response):
         
-        #Parse lens categories
+        #Parse products
 
-        rclens_categories = response.css('div.uk-clearfix > p a::attr(href)').extract()
+        avisual_products = response.css('ul.children a::attr(href)').getall()
         
-        for category_url in rclens_categories:
-            yield scrapy.Request(category_url, callback=self.parse_brands)
+        for i in avisual_products:
 
-    def parse_brands(self, response):
-        # Parse brands within categories
+            category_url = avisual_products[i]
 
-        lens_brands = response.css('div.uk-clearfix > p a::attr(href)').extract()
-        
-        for brand_url in lens_brands:
-            yield scrapy.Request(brand_url, callback=self.parse_products)
-
+            yield scrapy.Request(category_url, callback=self.parse_products)
+            
     def parse_products(self, response):
         # Parse product information within brands
 
-        products = response.css('article.uk-article')
+        products = response.css('a.woocommerce-loop-product__link')
 
         for product in products:
             # Extract product details and yield them or process as needed
 
             yield {
-                    'CATEGORY': 'LENSES',
+                    'CATEGORY': response.split('/')[4],
                     'BRAND': response.css('base').attrib['href'].split('/')[-1],
                     'TYPE': response.css('base').attrib['href'].split('/')[-2], 
                     'NAME': product.css('h1.uk-article-title > a ::text').get(),
@@ -41,3 +36,4 @@ class avisualLensSpider_1(scrapy.Spider):
                     'RENTAL': 'RC SERVICE',
                     'LINK': product.css('article.uk-article').attrib['data-permalink']
                     }
+
