c9afadd9ae84e77692af353a85f804aa6ab9ce5e
LeoBulcsu <miguelcuadrosmoronta@gmail.com>
2023-11-29 14:57:51 +0100

    "rclenses data scraped"

--- src/welabscraper/welabscraper/spiders/rcspider.py
+++ src/welabscraper/welabscraper/spiders/rcspider.py
@@ -2,33 +2,27 @@ import scrapy
 
 # RC SERVICE SCRAP
 
-class RCLensSpider(scrapy.Spider):
+class RCLensSpider_1(scrapy.Spider):
 
-    name = "rclensSpider"
+    name = "rclensSpider_1"
     start_urls = ["https://www.rcservice.es/es/alquiler-opticas-cine-peliculas"]
 
     def parse(self, response):
-        # Parse lens categories
+        
+        #Parse lens categories
 
         rclens_categories = response.css('div.uk-clearfix > p a::attr(href)').extract()
         
-        for i in range(len(rclens_categories)):
-
-            category_url = rclens_categories[i]
-            
+        for category_url in rclens_categories:
             yield scrapy.Request(category_url, callback=self.parse_brands)
 
     def parse_brands(self, response):
         # Parse brands within categories
-        
+
         lens_brands = response.css('div.uk-clearfix > p a::attr(href)').extract()
         
-        for i in range(len(lens_brands)):
-            
-            category_url = lens_brands[i]
-            
-            yield scrapy.Request(category_url, callback=self.parse_products)
-
+        for brand_url in lens_brands:
+            yield scrapy.Request(brand_url, callback=self.parse_products)
 
     def parse_products(self, response):
         # Parse product information within brands
@@ -37,13 +31,57 @@ class RCLensSpider(scrapy.Spider):
 
         for product in products:
             # Extract product details and yield them or process as needed
-            
+
             yield {
                     'CATEGORY': 'LENSES',
-                    'BRAND': product.css('base').attrib['href'].split('/')[-1],
-                    'TYPE': product.css('base').attrib['href'].split('/')[-2], 
-                    'NAME': product.css('h1.uk-article-title::text').get(),
+                    'BRAND': response.css('base').attrib['href'].split('/')[-1],
+                    'TYPE': response.css('base').attrib['href'].split('/')[-2], 
+                    'NAME': product.css('h1.uk-article-title > a ::text').get(),
                     'PRICE a day': 'Pedir presupuesto',
                     'RENTAL': 'RC SERVICE',
-                    'LINK': product.css('a.woocommerce-LoopProduct-link.woocommerce-loop-product__link').attrib['href']
+                    'LINK': product.css('article.uk-article').attrib['data-permalink']
+                    }
+
+
+
+
+
+class RCLensSpider_2(scrapy.Spider):
+
+    name = "rclensSpider_2"
+    start_urls = ["https://www.rcservice.es/es/alquiler-opticas-cine-peliculas"]
+
+    def parse(self, response):
+        
+        #Parse lens categories
+
+        rclens_categories = response.css('div.uk-clearfix > p a::attr(href)').extract()
+        
+        for category_url in rclens_categories:
+            yield scrapy.Request(category_url, callback=self.parse_products)
+
+    # def parse_brands(self, response):
+    #     # Parse brands within categories
+
+    #     lens_brands = response.css('div.uk-clearfix > p a::attr(href)').extract()
+        
+    #     for brand_url in lens_brands:
+    #         yield scrapy.Request(brand_url, callback=self.parse_products)
+
+    def parse_products(self, response):
+        # Parse product information within brands
+
+        products = response.css('article.uk-article')
+
+        for product in products:
+            # Extract product details and yield them or process as needed
+
+            yield {
+                    'CATEGORY': 'LENSES',
+                    'BRAND': product.css('h1.uk-article-title > a ::text').get().split(' ')[1],
+                    'TYPE': response.css('base').attrib['href'].split('/')[-1], 
+                    'NAME': product.css('h1.uk-article-title > a ::text').get(),
+                    'PRICE a day': 'Pedir presupuesto',
+                    'RENTAL': 'RC SERVICE',
+                    'LINK': product.css('article.uk-article').attrib['data-permalink']
                     }
