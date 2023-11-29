438af5ec0053a7bdc05022b72356fc5e430e1b57
LeoBulcsu <miguelcuadrosmoronta@gmail.com>
2023-11-29 19:00:56 +0100

    "ovide camera data"

--- src/welabscraper/welabscraper/spiders/ovidespider.py
+++ src/welabscraper/welabscraper/spiders/ovidespider.py
@@ -8,16 +8,56 @@ class OvideSpider(scrapy.Spider):
 
     def parse(self, response):
 
-        cameras = response.css('')
-
-        for camera in cameras:
+        for camera in response.css('div.product-grid-item'):
 
             yield {
                 'CATEGORY': 'CAMERAS',
-                'TYPE': 'Cine -Digital',
-                'BRAND': cameras.css('div.basel-product-brands-links > a ::text').get(), 
-                'NAME': cameras.css('h3.product-title > a ::text').get(),
+                'TYPE': 'Cine Digital',
+                'BRAND': camera.css('div.basel-product-brands-links > a ::text').get(), 
+                'NAME': camera.css('h3.product-title > a ::text').get(),
                 'PRICE a day': 'Pedir presupuesto',
                 'RENTAL': 'OVIDE',
-                'LINK': cameras.css('h3.product-title > a').attrib['href'] 
+                'LINK': camera.css('h3.product-title > a').attrib['href'] 
                 }
+
+
+class OvideSpider_2(scrapy.Spider):
+
+    name = 'ovidevideocameraSpider'
+    start_urls = ['https://www.ovide.com/alquiler/video/video-camaras/']
+
+    def parse(self, response):
+
+        for camera in response.css('div.product-grid-item'):
+
+            yield {
+                'CATEGORY': 'CAMERAS',
+                'TYPE': 'Video Digital',
+                'BRAND': camera.css('div.basel-product-brands-links > a ::text').get(), 
+                'NAME': camera.css('h3.product-title > a ::text').get(),
+                'PRICE a day': 'Pedir presupuesto',
+                'RENTAL': 'OVIDE',
+                'LINK': camera.css('h3.product-title > a').attrib['href'] 
+                }
+
+
+
+class OvideSpider(scrapy.Spider):
+
+    name = 'ovidelensSpider'
+    start_urls = ['https://www.ovide.com/alquiler/cine/objetivos/']
+
+    def parse(self, response):
+
+        for lens in response.css('div.product-grid-item'):
+
+            yield {
+                'CATEGORY': 'CAMERAS',
+                'TYPE': 'Cine Digital',
+                'BRAND': lens.css('div.basel-product-brands-links > a ::text').get(), 
+                'NAME': lens.css('h3.product-title > a ::text').get(),
+                'PRICE a day': 'Pedir presupuesto',
+                'RENTAL': 'OVIDE',
+                'LINK': lens.css('h3.product-title > a').attrib['href'] 
+                }
+
